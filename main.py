8192faa5f95f18b792a7eb6ba875500b4391c335
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 00:41:52 +0000

    catch, add and append total in loop for randompair

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ numbers1 = []
 numbers2 = []
 result = []
 for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
+	rand1 = random.randint(0,100)
+	rand2 = random.randint(0,100)
+	total = rand1 + rand2
+	numbers1.append(rand1)
+	numbers2.append(rand2)
+	result.append(total)
 print (numbers1)
 print (numbers2)
+print(result)
